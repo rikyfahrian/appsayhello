5adbaa839f661bffa204cd19726abad5334aa3be
rikyfahrian <rikyfhrian@gmail.com>
2022-11-21 02:20:19 +0700

    nih app ya anjng

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rikyfahrian/appsayhello
+
+go 1.19
+
+require github.com/rikyfahrian/sayhello2 v1.0.0
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	sayhello "github.com/rikyfahrian/sayhello2"
+)
+
+func main() {
+	riky := sayhello.SayHello()
+
+	fmt.Println(riky)
+}
